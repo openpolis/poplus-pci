ccc29918dae105b180535fda1e63781373343010
Guglielmo <guglielmo.celata@gmail.com>
2015-03-14 19:19:58 +0100

    version bumped to 0.1.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='poplus-pci',
-    version='0.1.8',
+    version='0.1.9',
     description='Generic python bindings to connect to the Poplus components',
     long_description=long_description,
     author='Openpolis',
