25199237447698420df269e6ea54e925eadf3188
Guglielmo <guglielmo.celata@gmail.com>
2015-03-29 16:55:20 +0200

    version bumped to 0.1.12

--- pci/__init__.py
+++ pci/__init__.py
@@ -1,4 +1,4 @@
-VERSION = '0.1.11'
+VERSION = '0.1.12'
 
 import copy
 from requests import ConnectionError
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='poplus-pci',
-    version='0.1.11',
+    version='0.1.12',
     description='Generic python bindings to connect to the Poplus components',
     long_description=long_description,
     author='Openpolis',
